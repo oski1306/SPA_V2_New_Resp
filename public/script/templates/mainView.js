258cec775aa34e64b93a24871290a02bcf007e44
Oskar <oski1306@o2.pl>
2023-10-02 22:20:22 +0200

    Attempt at creating to-do list functionality

--- public/script/templates/mainView.js
+++ public/script/templates/mainView.js
@@ -7,13 +7,30 @@ export default class extends AbstractView{
     async getPage(){
         return `
         <form class="logOut" action="/api/logout" method="post">
-           <button type="submit">Logout</button>
-        </form>
-           <div class="container">
-              <h1> Your To Do Lists </h1>
-              <button id="btn">Click Me</button>
-            </div>
+                <button type="submit">Logout</button>
+            </form>
+            <form id="taskForm">
+                <label for="task">Add Task</label>
+                <input type="text" id="task" name="task" required>
+                <button id="addTaskBtn">Add Task</button>
+            </form>
+            <div id="taskOutput"></div>
     `;
+    }
+    async addEventListeners(){
+        const taskForm = this.container.querySelector("#taskForm");
+        const taskOutput = this.container.querySelector("#taskOutput");
+        const addTaskBtn = this.container.querySelector("#addTaskBtn");
+
+        addTaskBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            const taskInput = taskForm.querySelector("#task");
+            const taskValue = taskInput.value;
+
+            taskOutput = `Task: ${taskValue}`;
+            taskInput.value = '';
+        } )
     }
 }
 
